e9ac5001734b78a3c81307a4d2d5ec4c4837e52e
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-06-13 17:33:47 +0900

    [level 2] Title: 더 맵게, Time: 1612.10 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,42 @@
+import java.util.*; 
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int num : scoville){
+            queue.add(num) ; 
+        }
+    
+        while(!queue.isEmpty()){
+            if(queue.peek() >= K){
+                break ;
+            }
+            else{
+                if(queue.size() >= 2){
+                    int num1 = queue.poll();
+                    int num2 = queue.poll();
+                    int range = num1 + (num2*2) ;
+                    queue.add(range);
+                    answer++;
+                }
+                else{
+                    break;
+                }
+                
+            }
+            
+        }
+        if(queue.size() == 1 && queue.poll() < K){
+           
+            return -1 ; 
+        }
+        else if(queue.size() == 1 && queue.poll() >= K){
+            return answer ;
+        }
+      
+        
+        return answer ; 
+        
+     
+    }
+}
