f9a8cff87453472e895bb4bad478695b624cf487
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-06-17 14:37:16 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.18 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,92 +1,40 @@
 import java.util.*;
 class Solution {
-    public int solution(String dartResult) {  
-        String num = "123456789";
-        int answer = 0;
-        int[] range = new int[3];
-        String[] strs = new String[3];
-        int count = 0 ;
-         for(int i = 0; i< dartResult.length() ; i++){
-            if(i>0){
-                if((dartResult.charAt(i-1) != '1' && dartResult.charAt(i) =='0')){
-                    range[count++] = i ;
-                } else if ((num.contains(dartResult.charAt(i)+""))) {
-                    range[count++] = i ;
+    public int solution(String dartResult) {
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+        for (int i = 0; i < dartResult.length(); ++i) {
+            char c = dartResult.charAt(i);
+            if (Character.isDigit(c)) {
+                sum = (c - '0');
+                if (sum == 1 && i < dartResult.length() - 1 && dartResult.charAt(i + 1) == '0') {
+                    sum = 10;
+                    i++;
                 }
-
-            }
-            else if(num.contains(dartResult.charAt(i)+"")||dartResult.charAt(i) == '0'){
-                    range[count++] = i;
+                stack.push(sum);
+            } else {
+                int prev = stack.pop();
+                if (c == 'D') {
+                    prev *= prev;
+                } else if (c == 'T') {
+                    prev = prev * prev * prev;
+                } else if (c == '*') {
+                    if (!stack.isEmpty()) {
+                        int val = stack.pop() * 2;
+                        stack.push(val);
+                    }
+                    prev *= 2;
+                } else if (c == '#') {
+                    prev *= (-1);
                 }
-        }
-
-
-        strs[0] = dartResult.substring(range[0] , range[1]);
-        strs[1] = dartResult.substring(range[1] , range[2]);
-        strs[2] = dartResult.substring(range[2] , dartResult.length());
-
-        List<Node> list = new ArrayList<>();
-        for(int i = 0 ; i<strs.length ; i++){
-            if(strs[i].charAt(strs[i].length()-1) == '*' ||strs[i].charAt(strs[i].length()-1) == '#'){
-                int number = Integer.parseInt(strs[i].substring(0 ,strs[i].length()-2));
-                if(strs[i].charAt(strs[i].length()-2) == 'S'){
-                    list.add(new Node(number ,strs[i].charAt(strs[i].length()-1) ));
-                } else if (strs[i].charAt(strs[i].length()-2) == 'D') {
-                    list.add(new Node(number*number ,strs[i].charAt(strs[i].length()-1) ));
-
-                }else if (strs[i].charAt(strs[i].length()-2) == 'T') {
-                    list.add(new Node(number*number*number ,strs[i].charAt(strs[i].length()-1) ));
-
-                }
-
-            }
-            else{
-                int number = Integer.parseInt(strs[i].substring(0 ,strs[i].length()-1));
-                if(strs[i].charAt(strs[i].length()-1) == 'S'){
-                    list.add(new Node(number));
-                } else if (strs[i].charAt(strs[i].length()-1) == 'D') {
-                    list.add(new Node(number*number));
-
-                }else if (strs[i].charAt(strs[i].length()-1) == 'T') {
-                    list.add(new Node(number*number*number));
-
-                }
+                // System.out.println(prev);
+                stack.push(prev);
             }
         }
-
-        for(int i= 0 ; i<list.size() ;i++){
-            if(list.get(i).opt == '#'){
-                list.get(i).num *= -1 ;
-            }
-            else if(list.get(i).opt == '*'){
-                list.get(i).num *=2 ;
-                if(i>0){
-                    list.get(i-1).num *= 2;
-                }
-            }
+        int totalScore = 0;
+        while (!stack.isEmpty()) {
+            totalScore += stack.pop();
         }
-
-        for (Node node : list) {
-            answer += node.num;
-        }
-        return answer;
-        //System.out.println(answer);
-        
-
+        return totalScore;
     }
-
-    static class Node{
-        int num ;
-        char opt ;
-
-        public Node(int num) {
-            this.num = num;
-        }
-
-        public Node(int num, char opt) {
-            this.num = num;
-            this.opt = opt;
-        }
-    }
-
 }
