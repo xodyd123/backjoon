c107ca1432e5c1fc0d9da21e1cd155c53b4f39eb
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-06-18 14:18:14 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.82 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,29 +1,109 @@
+import java.util.*;
 class Solution {
+    static StringBuilder sb ;
+    public String solution(String new_id){
+        String lowerCase = new_id.toLowerCase();
+        sb = new StringBuilder(lowerCase);
 
-    public String solution(String new_id) {
-        String answer = "";
-        //1
-        answer = new_id.toLowerCase();
-        //2
-        answer = answer.replaceAll("[^0-9a-z-_.]", "");
-        //3
-        answer = answer.replaceAll("\\.{2,}", ".");
-        //4
-        answer = answer.replaceAll("^[.]|[.]$", "");
-        //5
-        answer = answer.equals("") ? "a" : answer;
-        //6-1
-        answer = answer.length() >= 16 ? answer.substring(0, 15) : answer;
-        //6-2
-        answer = answer.replaceAll("[.]$", "");
-        //7
-        char lastCh = answer.charAt(answer.length() - 1);
-        if (answer.length() <= 2) {
-            while (answer.length() < 3) {
-                answer += lastCh;
+        check2();
+        check3();
+        check4();
+        check5();
+        check6();
+        check7();
+
+
+        //System.out.println(sb.toString());
+        return sb.toString();
+
+    }
+    private void check2() {
+        for(int i = 0; i< sb.toString().length() ; ++i){
+            char ch = sb.charAt(i);
+            if((ch - 'a') >=0 && (ch-'a') <=25){
+                continue;
+            }
+            if(Character.isDigit(ch)){
+                continue;
+            }
+            if(ch == '-'){
+                continue;
+            }
+            if(ch == '_'){
+                continue;
+            }
+            if(ch == '.'){
+                continue;
+            }
+            sb.deleteCharAt(i);
+            i = -1 ;
+        }
+    }
+
+      
+     private void check3(){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i<sb.length() ; i++){
+            stack.push(sb.charAt(i));
+            if(stack.size()>=2){
+                boolean bol = true ;
+                for(int j = 0 ; j<2 ; j++){
+                    if(stack.get(stack.size()-2+j) != '.'){
+                        bol = false;
+                        break;
+                    }
+                }
+
+                if(bol){
+                    for (int j = 0; j < 2; j++) {
+                        stack.pop();
+                    }
+                    stack.push('.');
+                }
+
+            }
+        }
+        StringBuilder sbs = new StringBuilder();
+        for (Character c : stack) {
+            sbs.append(c);
+
+        }
+        sb = sbs ;
+    }
+    private void check4(){
+        if(sb.length()>0 && sb.charAt(0) == '.'){
+            sb.deleteCharAt(0);
+        } if (sb.length()>0 && sb.charAt(sb.length()-1) == '.') {
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+
+    }
+    private void check5(){
+        if (sb.length() == 0) {
+            sb.append('a');
+
+        }
+
+    }
+
+    private void check6(){
+        if(sb.length()>=16){
+            sb.delete(15 , sb.length());
+            if(sb.charAt(sb.length()-1) == '.'){
+                sb.deleteCharAt(sb.length()-1);
             }
         }
 
-        return answer;
+
     }
+    private void check7(){
+        while (sb.length()<3){
+            sb.append(sb.charAt(sb.length()-1));
+        }
+        
+
+    }
+
+
 }
