92d99898ae338db23509698747e10b9cf009fe60
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-06-20 21:05:07 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.03 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,88 +1,50 @@
-import java.util.*;
 class Solution {
-       static final int[] arr  = {3,1,-1,-3};
+    //        0부터 9까지 좌표 {y,x}
+    int[][] numpadPos = {
+            {3,1}, //0
+            {0,0}, //1
+            {0,1}, //2
+            {0,2}, //3
+            {1,0}, //4
+            {1,1}, //5
+            {1,2}, //6
+            {2,0}, //7
+            {2,1}, //8
+            {2,2}  //9
+    };
+    //초기 위치
+    int[] leftPos = {3,0};
+    int[] rightPos = {3,2};
+    String hand;
+    public String solution(int[] numbers, String hand) {
+        this.hand = (hand.equals("right")) ? "R" : "L";
 
-      ArrayDeque<Integer> queue = new ArrayDeque<>() ;
-    public String solution(int[] numbers , String hande) {
-        StringBuilder sb = new StringBuilder();
-        int left = 10 ;
-        int right = 12 ;
+        String answer = "";
+        for (int num : numbers) {
+            String Umji = pushNumber(num);
+            answer += Umji;
 
-        for(int num : numbers){
-            int[] left_dict = new int[13];
-            int[] right_dict = new int[13];
-            if(num == 1 || num == 4 || num == 7){
-                left = num ;
-                sb.append('L');
-            } else if (num == 3 || num == 6 || num == 9) {
-                right = num ;
-                sb.append('R');
-            } else{
-                int dict = num ;
-                if(num == 0){
-                    dict = 11 ;
-                }
-                queue.add(left);
-                if(left == dict){
-                   left_dict[dict] = 0;
-                }
-                else{
-                    while (!queue.isEmpty()){
-                        Integer before = queue.poll();
-                        for(int i = 0  ; i<4 ; i++){
-                            int after = before + arr[i];
-                            if(after<0 || after>12){
-                                continue;
-                            }
-                            if(left_dict[after] == 0){
-                                left_dict[after] = left_dict[before] +1 ;
-                                queue.add(after) ;
-                            }
-                        }
-                    }
-                }
-                if(right == dict){
-                    right_dict[dict] = 0 ;
-                }
-                else{
-                    queue.add(right);
-                    while (!queue.isEmpty()){
-                        Integer before = queue.poll();
-                        for(int i = 0  ; i<4 ; i++){
-                            int after = before + arr[i];
-                            if(after<0 || after>12){
-                                continue;
-                            }
-                            if(right_dict[after] == 0){
-                                right_dict[after] = right_dict[before] +1 ;
-                                queue.add(after) ;
-                            }
-                        }
-                    }
-                }
-
-                if(right_dict[dict] == left_dict[dict]){
-                    if(hande.equals("right")){
-                        sb.append("R");
-                        right = dict;
-                    }
-                    else{
-                        sb.append("L");
-                        left = dict;
-                    }
-                } else if (left_dict[dict] > right_dict[dict]) {
-                    sb.append("R");
-                    right = dict ;
-                }
-                else{
-                    sb.append("L");
-                    left = dict ;
-                }
-
-            }
-
-
+            if(Umji.equals("L")) {leftPos = numpadPos[num]; continue;}
+            if(Umji.equals("R")) {rightPos = numpadPos[num]; continue;}
         }
-        return sb.toString();
+        return answer;
+    }
+
+    //num버튼을 누를 때 어디 손을 사용하는가
+    private String pushNumber(int num) {
+        if(num==1 || num==4 || num==7) return "L";
+        if(num==3 || num==6 || num==9) return "R";
+
+        // 2,5,8,0 일때 어디 손가락이 가까운가
+        if(getDist(leftPos, num) > getDist(rightPos, num)) return "R";
+        if(getDist(leftPos, num) < getDist(rightPos, num)) return "L";
+
+        //같으면 손잡이
+        return this.hand;
+    }
+
+    //해당 위치와 번호 위치의 거리
+    private int getDist(int[] pos, int num) {
+        return Math.abs(pos[0]-numpadPos[num][0]) + Math.abs(pos[1]-numpadPos[num][1]);
     }
 }
