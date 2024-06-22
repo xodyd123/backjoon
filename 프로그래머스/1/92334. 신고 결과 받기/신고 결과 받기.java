a6609272f79b9a4ba3e9160c21be8bbcfe15c6ab
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-06-22 20:58:33 +0900

    [level 1] Title: 신고 결과 받기, Time: 13.21 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,48 +1,21 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Solution {
-
     public int[] solution(String[] id_list, String[] report, int k) {
-      Set<String>[] list = new HashSet[id_list.length];
-      Map<String , Integer> users = new HashMap<>();
-      for(int i = 0 ; i<list.length ; i++){
-          list[i] = new HashSet<>();
-          users.put(id_list[i] , 0 );
-      }
-
-      for(String str : report){
-          String[] strings = str.split(" ");
-          for(int i = 0 ; i<id_list.length ; i++){
-              if(id_list[i].equals(strings[0])){
-                  list[i].add(strings[1]);
-              }
-          }
-      }
-
-      for(int i = 0 ; i<list.length ; i++){
-          Iterator<String> it = list[i].iterator();
-          while (it.hasNext()){
-              String next = it.next();
-              users.put(next , users.get(next)+1);
-          }
-      }
-
-        int[] result = new int[id_list.length];
-        for (int i = 0; i < id_list.length; i++) {
-            Iterator<String> iterator = list[i].iterator();
-            int num = 0 ;
-            while (iterator.hasNext()){
-                String next = iterator.next();
-                if(users.get(next) >=k ){
-                    num++;
-                }
-            }
-            result[i] = num ;
+        List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
+        HashMap<String, Integer> count = new HashMap<>();
+        for (String s : list) {
+            String target = s.split(" ")[1];
+            count.put(target, count.getOrDefault(target, 0) + 1);
         }
 
-        return result ;
-
+        return Arrays.stream(id_list).map(_user -> {
+            final String user = _user;
+            List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
+            return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
+        }).mapToInt(Long::intValue).toArray();
     }
-
-    
-
 }
