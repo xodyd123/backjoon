30e154a4889d7256bc82085667a3e26602ac161b
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-06-24 20:27:36 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.95 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,49 +1,27 @@
-import java.util.ArrayDeque;
 import java.util.Stack;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        ArrayDeque<Integer>[] arr = new ArrayDeque[board.length+1];
-        for (int i = 0; i < board.length+1; i++) {
-            arr[i] = new ArrayDeque<>();
-        }
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] != 0) {
-                    arr[j+1].add(board[i][j]);
-                }
-            }
-        }
-
+        int answer = 0;
         Stack<Integer> stack = new Stack<>();
-        int count = 0 ;
-        for (int i = 0; i < moves.length; i++) {
-            int move = moves[i];
-            Integer poll = arr[move].poll();
-            if(poll == null || poll == 0){
-                continue;
-            }
-        
-
-            stack.push(poll);
-            boolean bol = false ;
-            if (stack.size()>=2) {
-                Integer pop1 = stack.pop();
-                Integer pop2 = stack.pop();
-                if(pop1 != pop2 ){
-                    bol = true ;
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.isEmpty()) {
+                        stack.push(board[j][move - 1]);
+                        board[j][move - 1] = 0;
+                        break;
+                    }
+                    if (board[j][move - 1] == stack.peek()) {
+                        stack.pop();
+                        answer += 2;
+                    } else
+                        stack.push(board[j][move - 1]);
+                    board[j][move - 1] = 0;
+                    break;
                 }
-                if(bol){
-                    stack.push(pop2);
-                    stack.push(pop1);
-                    continue;
-                }
-                count+=2;
-
             }
-          
         }
-
-        return count ;
+        return answer;
     }
 }
