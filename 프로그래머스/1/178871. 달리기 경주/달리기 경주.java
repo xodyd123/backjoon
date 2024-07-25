8551bd23ac1327d3c6e38c90c65b73572bbb3a1f
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-07-25 17:10:31 +0900

    [level 1] Title: 달리기 경주, Time: 566.58 ms, Memory: 373 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,41 +1,26 @@
 import java.util.*;
 
 class Solution {
+    static Map<String , Integer> map ;
     public String[] solution(String[] players, String[] callings) {
-		String[] answer = new String[players.length];
+        map = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i] , i);
+        }
+        for (String calling : callings) {
+            int num = map.get(calling);
+            if(num>0){
+                String range = players[num-1];
+                players[num-1] = players[num] ;
+                players[num] = range ;
+                map.replace(players[num] , num);
+                map.replace(players[num-1] , num-1);
+            }
 
-		HashMap<String, Integer> mappedByPlayer = new HashMap<>();
-		HashMap<Integer, String> mappedByRank = new HashMap<>();
+        }
+        return players ;
+    }
 
-		// 각각의 맵을 초기화
-		for (int i = 0; i < players.length; i++) {
-			mappedByPlayer.put(players[i], i);
-			mappedByRank.put(i, players[i]);
-		}
 
-		for (int i = 0; i < callings.length; i++) {
-
-			// 추월한 유저 순위
-			// 추월한 유저 이름
-			int currentRank = mappedByPlayer.get(callings[i]);
-			String player = mappedByRank.get(currentRank);
-
-			// 바로 앞 플레이어
-			String frontPlayer = mappedByRank.get(currentRank - 1);
-
-			// swap
-			mappedByPlayer.put(player, currentRank - 1);
-			mappedByPlayer.put(frontPlayer, currentRank);
-
-			mappedByRank.put(currentRank - 1, player);
-			mappedByRank.put(currentRank, frontPlayer);
-		}
-
-		for (int i = 0; i < players.length; i++) {
-			answer[i] = mappedByRank.get(i);
-		}
-
-		return answer;
-	}
-        
 }
+        
