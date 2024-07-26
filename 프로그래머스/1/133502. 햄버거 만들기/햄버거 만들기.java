e71f3018c016899eb226f59168271ea8a761ee3e
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-07-26 17:49:55 +0900

    [level 1] Title: 햄버거 만들기, Time: 32.75 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,23 +1,19 @@
 import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
-       int answer = 0;
-		List<Integer> li = new ArrayList<>();
-		
-		for(int i :ingredient) {
-			li.add(i);
-			while(li.size() >= 4) {
-				int n = li.size();
-				if(!(li.get(n-1) == 1
-					&& li.get(n-2)==3
-					&& li.get(n-3) ==2
-					&& li.get(n-4)==1)) break;
-				for(int j=0; j<4; j++) {
-					li.remove(li.size() -1);
-				}
-				answer++;
-			}
-		}
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
+        int answer = 0;
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
+            }
+        }
         return answer;
     }
 }
