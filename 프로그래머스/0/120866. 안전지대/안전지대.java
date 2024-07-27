2c02134b57997649321cb0cfd18a50a400bb0389
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-07-27 13:05:38 +0900

    [level 0] Title: 안전지대, Time: 1.97 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,51 +1,42 @@
-import java.util.*; 
 class Solution {
-    private static final int[] x_range = {1,-1 , 0 , 0 , 1 ,-1 , 1 , -1};
-    private static final int[] y_range = {0,0 , 1 , -1 , 1 ,-1 , -1  ,1};
-    List<Node> list = new ArrayList<>();
     public int solution(int[][] board) {
-        int size = board.length * board[0].length; 
-        for(int i = 0 ; i<board.length ; i++){
-            for(int j = 0 ; j<board[i].length ; j++){
-                if(board[i][j] == 1){
-                    list.add(new Node(i,j));
+        int answer = 0;
+        int length = board.length;   //길이
+        int[][] temp = new int[length+2][length+2];
+        // 길이를 2 늘린 액자용 배열 생성 -> 이러면 단순한 조건식으로 안전영역 구할 수 있음
+
+        // 액자에 board 이식.
+        for(int i=1; i<length+1; i++){
+            for(int j=1; j<length+1;j++){
+                temp[i][j]=board[i-1][j-1];
+            }
+        }
+
+
+
+        //위험지대 찾기
+        for(int i=1; i<length+1; i++){
+            for(int j=1; j<length+1;j++){
+                if(temp[i][j]==1){
+                    for(int a = i-1; a<=i+1; a++){
+                        for(int b =j-1; b<=j+1; b++){
+                            if(temp[a][b]!=1) temp[a][b]=2;
+                        }
                     }
                 }
             }
-        
-        for(Node node : list){
-            for(int i = 0 ; i<x_range.length ; i++){
-                int x = node.x+x_range[i];
-                int y = node.y+y_range[i];
-                if(x>=board.length || y>=board[0].length || x<0 || y<0){
-                    continue ;
-                }
-                if(board[x][y] == 1){
-                    continue;
-                }
-                
-                board[x][y] = 1 ; 
-            }
         }
-        int answer = 0 ; 
-        for(int j =0  ; j<board.length ; j++){
-            for(int i = 0 ; i<board[j].length ; i++){
-                if(board[i][j] == 0){
-                    answer++;
-                }
+
+        // 안전지대 카운트
+        for(int i=1; i<length+1; i++){
+            for(int j=1; j<length+1;j++){
+                if(temp[i][j]==0) answer++;
+                System.out.print(temp[i][j]);
             }
-        }    
+            System.out.println("");
+        }
+
+
         return answer;
     }
-static class Node{
-    int x ; 
-    int y ; 
-    
-    Node(int x , int y){
-        this.x = x;
-        this.y = y ;
-    }
 }
-
-}
-
