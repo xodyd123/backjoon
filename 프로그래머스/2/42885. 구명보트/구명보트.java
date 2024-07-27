d9a2e5c6f70f3415fe1005b59fa8b8d2e163663d
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-07-27 22:00:28 +0900

    [level 2] Title: 구명보트, Time: 12.30 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,25 +1,12 @@
-import java.util.*;
+import java.util.Arrays;
 class Solution {
-     public int solution(int[] people, int limit) {
+    public int solution(int[] people, int limit) {
         Arrays.sort(people);
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        for (int person : people) {
-            deque.add(person);
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
         }
-        int count = 0 ;
-        while (deque.size()>1) {
-            Integer last  = deque.pollLast();
-            Integer first  = deque.pollFirst();
-            if(first+last > limit){
-                count++ ;
-                deque.addFirst(first);
-            } else if (first+last <= limit) {
-                count++;
-            }
-        }
-        if(deque.size() == 1 ){
-            count++ ; 
-        }
-        return count;
+        return people.length - i;
     }
 }
