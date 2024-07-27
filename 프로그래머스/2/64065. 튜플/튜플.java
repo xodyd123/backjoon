03cb5dc8f69f808e6d68a8ef750432ba785aea14
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-07-27 14:05:17 +0900

    [level 2] Title: 튜플, Time: 58.83 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,36 +1,16 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        Set<String> set = new LinkedHashSet<>();
-        s = s.substring(0 , s.length() -2).replace("{" , "");
-        String[] arr = s.split("},");
-        Arrays.sort(arr , new Comparator<String>(){
-            @Override
-            public int compare(String o1 , String o2){
-                if(o1.length() > o2.length()){
-                    return 1 ; 
-                }
-                if(o2.length() > o1.length()){
-                    return - 1; 
-                }
-                return 0 ; 
+        Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
             }
-            
-        });
-        
-        for(int i = 0 ; i<arr.length ; i++){
-            String[] strs = arr[i].split(","); 
-            for(int j = 0 ; j<strs.length ; j++){
-                set.add(strs[j]);
-            }
-        } 
-        
-        List<String> list = new ArrayList<>(set);
-        int[] answer = new int[list.size()];
-        for(int i = 0 ; i<list.size() ; i++){
-            answer[i] = Integer.parseInt(list.get(i)); 
         }
-
         return answer;
     }
 }
