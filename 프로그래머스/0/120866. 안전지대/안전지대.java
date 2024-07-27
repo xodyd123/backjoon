171d4cf52df2c4ec88fb6c6cf61058eaa990ee5b
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-07-27 13:04:23 +0900

    [level 0] Title: 안전지대, Time: 1.36 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,6 +1,51 @@
+import java.util.*; 
 class Solution {
+    private static final int[] x_range = {1,-1 , 0 , 0 , 1 ,-1 , 1 , -1};
+    private static final int[] y_range = {0,0 , 1 , -1 , 1 ,-1 , -1  ,1};
+    List<Node> list = new ArrayList<>();
     public int solution(int[][] board) {
-        int answer = 0;
+        int size = board.length * board[0].length; 
+        for(int i = 0 ; i<board.length ; i++){
+            for(int j = 0 ; j<board[i].length ; j++){
+                if(board[i][j] == 1){
+                    list.add(new Node(i,j));
+                    }
+                }
+            }
+        
+        for(Node node : list){
+            for(int i = 0 ; i<x_range.length ; i++){
+                int x = node.x+x_range[i];
+                int y = node.y+y_range[i];
+                if(x>=board.length || y>=board[0].length || x<0 || y<0){
+                    continue ;
+                }
+                if(board[x][y] == 1){
+                    continue;
+                }
+                
+                board[x][y] = 1 ; 
+            }
+        }
+        int answer = 0 ; 
+        for(int j =0  ; j<board.length ; j++){
+            for(int i = 0 ; i<board[j].length ; i++){
+                if(board[i][j] == 0){
+                    answer++;
+                }
+            }
+        }    
         return answer;
     }
+static class Node{
+    int x ; 
+    int y ; 
+    
+    Node(int x , int y){
+        this.x = x;
+        this.y = y ;
+    }
 }
+
+}
+
