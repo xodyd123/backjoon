88b7e092a9d674138cec526f8116216629da428e
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-07-28 17:18:13 +0900

    [level 2] Title: 기능개발, Time: 0.50 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,30 +1,37 @@
 import java.util.*;
-class Solution { 
+class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>(); 
-        List<Integer> result = new ArrayList<>();
-        for(int i = 0 ; i<progresses.length ; i++){
-            if((100-progresses[i]) % speeds[i] == 0) {
-                 list.add((100-progresses[i]) / speeds[i]) ;
-            }
-            else {
-                list.add((100-progresses[i])/speeds[i] + 1) ;
-                
-            }
+
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < progresses.length; i++) {
+            double d = (100-progresses[i]) / (double) speeds[i];
+            int range =(int)Math.ceil(d);
+            queue.add(range);
         }
-    for(Integer i : list){
-        if(q.isEmpty()){q.add(i);}
-        else if(!q.isEmpty() && q.peek()>=i){q.add(i);}
-        else if(!q.isEmpty() && q.peek()<i){result.add(q.size()); 
-                                          q.clear();
-                                          q.add(i);}
-        
-    } 
-    if(q.size()>0){result.add(q.size());}
+        // queue 세팅
+        int count  =  1 ;
+        int days = queue.poll();
+        List<Integer> list = new ArrayList<>(); // 7, 3 , 9
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+            if(days>= poll){
+                count++;
+            }
+            else{
+                list.add(count);
+                days = poll ;
+                count = 1 ;
+            }
 
+        }
+        if (count >= 1) {
+            list.add(count);
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
 
- 
-    System.out.println(result);
-    return result.stream().mapToInt(Integer::intValue).toArray();}
+        return answer;
+    }
 }
