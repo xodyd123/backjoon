3681bb86bff7ae67680776b219a6454be2a82fe9
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-12-21 00:38:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/README.md
+++ 프로그래머스/1/76501. 음양 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 음양 더하기 - 76501 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.30 ms
+메모리: 33.3 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 21일 00:21:51
+2024년 12월 21일 00:38:43
 
 ### 문제 설명
 
--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,8 +1,8 @@
 function solution(absolutes, signs) {
-    var answer = 0
-    const arr = absolutes.map((arr , index) => {
-        return signs[index] ? arr : -arr ;  
-    } );
-    arr.forEach(arr => answer += arr);
+  
+    const answer = absolutes.reduce((inital , current  , index) => {
+    
+        return signs[index] ? inital+current : inital-current ; 
+    } , 0);
     return answer;
 }
