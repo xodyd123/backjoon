b74b030cc86da8d1f14db9f102d3fb727fcbc7a6
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-12-20 14:59:01 +0900

    [level 1] Title: 약수의 합, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    for(let num = 1 ; num<=n ; num++){
+        if(n % num == 0){
+            answer += num ; 
+        }
+    }
+    return answer;
+}
+
