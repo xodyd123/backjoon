1cf059c1965365307d0348f8d376679c0e923856
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-12-24 16:46:50 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,20 +1,11 @@
 function solution(s) {
-    const zero = /zero/g ; 
-    const one = /one/g ; 
-    const two = /two/g ; 
-    const three = /three/g ; 
-    const four = /four/g ; 
-    const five = /five/g ; 
-    const six = /six/g ; 
-    const seven = /seven/g ; 
-    const eight = /eight/g ; 
-    const nine = /nine/g ; 
-    
-    const arr=[zero , one , two , three ,four ,five ,six ,seven , eight , nine]; 
-    
-    arr.forEach((num , index) => {
-      
-        s= s.replace(num , index); 
-    });
-    return Number(s) ; 
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
 }
