2f63d184ef39d0d35293b10b0bea6a6ac420a0be
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-12-24 14:44:06 +0900

    [level 1] Title: K번째수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,22 @@
+function solution(array, commands) {
+    var answer = [];
+    for(const num of commands){
+        let result = check(array , num); 
+        answer.push(result); 
+    }
+    return answer;
+}
+
+
+function check(array , num){
+  
+    let [num1 , num2 , num3] = num ; 
+    
+    let num4 ; 
+    
+    num1 > num2 ? (num4 = num1 , num1 = num2 , num2 = num4) : num1 ;  
+    
+    let newArr = array.slice(num1-1 , num2).sort((result1 , result2) => result1 - result2  ); 
+ 
+    return newArr[num3-1] ; 
+}
