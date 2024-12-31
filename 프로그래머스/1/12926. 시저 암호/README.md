948df1cdb7b4632681d614858f9cf124ad69a6b2
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2024-12-31 15:48:46 +0900

    [level 1] Title: 시저 암호, Time: 5.39 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 34 MB, 시간: 4.68 ms
+메모리: 37.6 MB, 시간: 5.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 31일 15:44:39
+2024년 12월 31일 15:48:44
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,16 +1,15 @@
 function solution(s, n) {
-    const str1 = "abcdefghijklmnopqrstuvwxyz";
-    const str2 = str1.toUpperCase(); 
-    let arr1 = str1.split(""); 
-    let arr2 = str2.split("") ; 
-    arr1 = [...arr1 , ...arr1] ;
-    arr2= [...arr2 , ...arr2]; 
+    let str1 = "abcdefghijklmnopqrstuvwxyz";
+    let str2 = str1.toUpperCase(); 
+    str1 += str1 ;
+    str2 += str2 ; 
+  
     var answer = '';
     
     for(const i of s){
-        if(arr1.indexOf(i) !== -1) answer += arr1[arr1.indexOf(i)+n]; 
+        if(str1.indexOf(i) !== -1) answer += str1[str1.indexOf(i)+n]; 
         
-        else if(arr2.indexOf(i) !== -1) answer += arr2[arr2.indexOf(i)+n];
+        else if(str2.indexOf(i) !== -1) answer += str2[str2.indexOf(i)+n];
         
         else answer += i; 
     }
