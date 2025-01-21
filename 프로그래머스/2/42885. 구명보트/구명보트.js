69b3aedcab1449ef4f048e1d74c39a730f0d62d2
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-01-21 17:19:04 +0900

    [level 2] Title: 구명보트, Time: 13.00 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,14 +1,19 @@
 function solution(people, limit) {
-  let biggest = 0,
-    count = 0,
-    i = 0;
-  people.sort((a, b) => a - b);
-  while (people.length > 0) {
-    biggest = people.pop();
-    i = 0;
-    while (people[i] <= limit - biggest) i++;
-    if (i) people.splice(i - 1, 1);
-    count++;
-  }
-  return count;
+    // 몸무게 배열을 내림차순 정렬
+    people.sort((a, b) => b - a);
+
+    let answer = 0;
+    let left = 0; // 가장 가벼운 사람의 인덱스
+    let right = people.length - 1; // 가장 무거운 사람의 인덱스
+
+    while (left <= right) {
+        // 가장 무거운 사람과 가장 가벼운 사람의 조합 확인
+        if (people[left] + people[right] <= limit) {
+            right--; // 가장 가벼운 사람 구출
+        }
+        left++; // 가장 무거운 사람 구출
+        answer++; // 보트 사용
+    }
+
+    return answer;
 }
