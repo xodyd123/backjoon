9726cbeccae993f3fdc5b0ef75aee8bab8f029e7
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-02-01 02:24:49 +0900

    [level 1] Title: 폰켓몬, Time: 0.66 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -2,8 +2,7 @@ function solution(nums) {
     const count = [...new Set(nums)];
     
     
-    
-    console.log(count); 
+
     var answer = 0;
     
     return nums.length/2<count.length ? nums.length/2 : count.length ; 
