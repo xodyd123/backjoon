9fa326c5d62fe8ffb9409e11e802f4f9e7804d3c
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-01-31 16:12:32 +0900

    [level 2] Title: 할인 행사, Time: 20.58 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 52.8 MB, 시간: 178.47 ms
+메모리: 41.9 MB, 시간: 20.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 31일 16:06:17
+2025년 01월 31일 16:12:30
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,29 +1,16 @@
 function solution(want, number, discount) {
-    var answer = 0;
-    for(let i = 0 ; i<want.length ; i++){
-        obj[want[i]] = number[i] ; 
-    }
-    let sum = number.reduce((item , cur) => item + cur) ;
-    for(let i = 0 ; i<=discount.length - sum ; i++){
-        let newObj = Object.assign({} ,obj) ; 
- 
-        let slice = discount.slice(i , sum+i) ;
-        for(let str of slice){ 
-            newObj[str] = newObj[str] -1 ;  
-        }      
-        let set = new Set(Object.values(newObj)) ;
-       ;
-        if(set.size === 1 && set.has(0)){
-            answer ++ ; 
+    let count = 0;
+    for (let i = 0; i < discount.length - 9; i++) {
+        const slice = discount.slice(i, i+10);
+
+        let flag = true;
+        for (let j = 0; j < want.length; j++) {
+            if (slice.filter(item => item === want[j]).length !== number[j]) {
+                flag = false;
+                break;
+            }
         }
-    } 
-   
-    return answer;
-}
-
-
-let obj =  {
-    
-    
-   
+        if (flag) count += 1;
+    }
+    return count;
 }
