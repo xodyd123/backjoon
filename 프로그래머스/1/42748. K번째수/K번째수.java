c4bd732d631d66572584e1c8d836ae63d9b3f1e0
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-02-04 16:32:01 +0900

    [level 1] Title: K번째수, Time: 2.74 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,16 +2,15 @@ import java.util.*;
 class Solution {
     List<Integer> arr = new ArrayList<>() ; 
     
-    public List<Integer> solution(int[] array, int[][] commands) {
+    public int[] solution(int[] array, int[][] commands) {
       
         for(int[] arr : commands) {
             int start = Math.min(arr[0] - 1 , arr[1]) ;
             int end = Math.max(arr[0] - 1 , arr[1]) ;
             func(start , end , arr[2]-1 , array) ;
         }
-        
-    
-        return arr ;
+        int[] result = arr.stream().mapToInt(Integer::intValue).toArray();
+        return result ;
     }
     
     public void func(int start , int end , int result , int[] array){
