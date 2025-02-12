038399d1e5a2d406148a8a766f5f3dcde825452a
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-02-12 14:50:38 +0900

    [level 3] Title: 가장 먼 노드, Time: 39.48 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,66 @@
+import java.util.*; 
+class Solution {
+    private List<Integer>[] list ; 
+    private boolean[] bol ; 
+    private List<Integer> result = new ArrayList<>() ; 
+    private ArrayDeque<Node> deque = new ArrayDeque<>(); 
+    public int solution(int n, int[][] edge) {
+        list = new ArrayList[n+1] ; 
+         bol = new boolean[n+1] ; 
+        
+        for(int i = 0 ; i<= n ; i++){
+            list[i] = new ArrayList<>() ; 
+        }
+        
+        for(int[] arr : edge){
+           list[arr[0]].add(arr[1]);
+           list[arr[1]].add(arr[0]);
+            
+        }
+       
+        bol[1] = true ; 
+        deque.add(new Node(1,1)) ; 
+        bfs(); 
+        Collections.sort(result, Collections.reverseOrder()) ;
+        int max = result.get(0); 
+        int count = 0 ; 
+        for(int num : result){
+            if(max == num){
+                count ++ ; 
+            }
+        }
+        
+       
+        return count ; 
+    }
+    
+    private void bfs(){
+        
+        while(!deque.isEmpty()){
+            Node node = deque.poll() ;
+            int start = node.start ; 
+            int dict = node.dict ; 
+       for(int end : list[start]){
+           if(!bol[end]){
+               bol[end] = true ;
+               Node newNode = new Node(end , dict+1) ; 
+               result.add(dict+1) ; 
+               deque.add(newNode) ; 
+           }
+       }
+      }
+        
+      
+    }
+    
+    static class Node {
+        int start ;
+        int dict ; 
+        
+        Node(int start , int dict){
+            this.start = start ; 
+            this.dict = dict ; 
+        };
+    }
+}
+
