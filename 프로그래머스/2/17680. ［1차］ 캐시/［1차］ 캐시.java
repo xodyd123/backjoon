e904760975e94696340735dfc265e4218e0f7968
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-02-14 01:13:32 +0900

    [level 2] Title: [1차] 캐시, Time: 45.18 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,8 +1,7 @@
 import java.util.*; 
-
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        if (cacheSize == 0) return cities.length * 5; 
+        if (cacheSize == 0) return cities.length * 5; // 캐시 크기가 0이면 바로 반환
         
         Db db = new Db(cacheSize);
         for (String str : cities) {
@@ -13,7 +12,7 @@ class Solution {
     }
 
     static class Db {
-        ArrayDeque<String> deque = new ArrayDeque<>();
+        Queue<String> deque = new LinkedList<>();
         int time;
         int size;
 
@@ -23,12 +22,10 @@ class Solution {
 
         private void add(String str) {
             if (check(str)) {
-
                 deque.remove(str);
                 deque.add(str);
                 time += 1;
             } else {
-              
                 if (deque.size() >= size) {
                     deque.poll();
                 }
