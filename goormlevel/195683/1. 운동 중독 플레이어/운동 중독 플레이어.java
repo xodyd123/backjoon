92b8916c7472e5cce1596d0f8f9af57a785fabbe
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-02-23 15:44:56 +0900

    [난이도 1] Title: 운동 중독 플레이어, Time: 0.07 ms, Memory: 31.51 MB -BaekjoonHub

---
+++ goormlevel/195683/1. 운동 중독 플레이어/운동 중독 플레이어.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" "); 
+		int w = Integer.parseInt(input[0]) ;
+		int r = Integer.parseInt(input[1]); 
+		
+		int result = (int)(w * (1+ (r/30.0)));  
+		System.out.println(result);
+	}
+}
