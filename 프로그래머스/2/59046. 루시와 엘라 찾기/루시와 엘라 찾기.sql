c27b0d488c50dd2ef7d7d77979b0fc03355833ff
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-03-07 14:40:02 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,3 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID , NAME	, SEX_UPON_INTAKE from ANIMAL_INS 
+where name = "Ella" or name = "Lucy" or name = "Pickle" or name = "Rogan" or name = "Sabrina" or name = "Mitty" ; 
