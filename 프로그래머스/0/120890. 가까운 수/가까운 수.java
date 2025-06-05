d968f88695db3521b35488d2a0acebc15f5d6cff
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-06-05 13:59:23 +0900

    [level 0] Title: 가까운 수, Time: 0.50 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+class Solution {
+    public int solution(int[] array, int n) {
+        Arrays.sort(array) ; 
+        int result = 100 ;  
+        int answer = 0 ; 
+        for(int num : array){
+            int range = Math.abs(num-n) ;
+            if(result > range) {
+                result = range ; 
+                answer = num ; 
+                
+              
+            }
+        }
+        return answer ; 
+       
+    }
+}
