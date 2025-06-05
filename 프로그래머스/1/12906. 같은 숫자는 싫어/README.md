a52e5a4a3e3765a1bda78a2ddec463ef20a0fd1d
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-06-05 15:30:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.71 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 116 MB, 시간: 49.15 ms
+메모리: 117 MB, 시간: 44.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 16:41:36
+2025년 06월 05일 15:30:51
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,14 +2,28 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
+   
+        ArrayDeque<Integer> que = new ArrayDeque<>();
+        for(int num : arr){
+           if(que.isEmpty()){
+               que.add(num) ; 
+           } 
+           else if(que.peekLast() == num){
+               continue ; 
+           }
+          else {
+              que.add(num) ; 
+          }
+        }
+          int[] answer = new int[que.size()];
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+       int num = 0 ; 
+       while(!que.isEmpty()){
+           answer[num++] = que.poll() ;
+       }
+        
+       
 
-        ArrayDeque<Integer> que = new ArrayDeque<>() ; 
-        for(int i : arr){
-          if(!que.isEmpty() && que.peekLast() == i) continue ;
-          
-            que.add(i) ; 
-        }        
-
-        return que.stream().mapToInt(Integer::new).toArray();
+        return answer;
     }
 }
