71455091f75eae133914adf224489254096f6062
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-06-08 15:16:05 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 16.43 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -3,12 +3,9 @@ class Solution {
     public int[] solution(String s) {
         Map<String , Integer> map = new HashMap<>() ;
         String[] str = s.split("") ; 
-        
-        System.out.println(Arrays.toString(str));
+       
         int[] answer = new int[s.length()] ;
-        for(int i = 0 ; i<answer.length ; i++){
-            answer[i] = -1 ; 
-        }
+        Arrays.fill(answer , -1) ; 
         
         for(int i= 0 ; i<str.length ; i++){
            if(map.containsKey(str[i])){
