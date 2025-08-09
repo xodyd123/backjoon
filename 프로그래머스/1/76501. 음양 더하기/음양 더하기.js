87c7b18e85b81bfedc198ca2ee1398970cc753d4
Jeong Tae Yong <115709406+xodyd123@users.noreply.github.com>
2025-08-09 11:26:50 +0900

    [level 1] Title: 음양 더하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,8 +1,8 @@
 function solution(absolutes, signs) {
   
-    const answer = absolutes.reduce((inital , current  , index) => {
+    const answer = absolutes.reduce((inital , current  , index) => 
     
-        return signs[index] ? inital+current : inital-current ; 
-    } , 0);
+         signs[index] ? inital+current : inital-current 
+     , 0);
     return answer;
 }
